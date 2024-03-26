128cf70da4b52a263f29f5a85a20b19dbfe36be4
ndjerrou <n.djerrou@gmail.com>
2024-03-26 11:26:59 +0000

    send truthy props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import UserList from './components/UserList';
 
 export default function App() {
   const users = [
-    { id: 1, name: 'Patrick', city: 'Paris' },
-    { id: 2, name: 'Jean', city: 'Marseille' },
-    { id: 3, name: 'Noé', city: 'Puteaux' },
-    { id: 4, name: 'Rachid', city: 'Cergy' },
+    { id: 1, name: 'Patrick', city: 'Paris', isMajor: true },
+    { id: 2, name: 'Jean', city: 'Marseille', isMajor: true },
+    { id: 3, name: 'Noé', city: 'Puteaux', isMajor: false },
+    { id: 4, name: 'Rachid', city: 'Cergy', isMajor: false },
   ];
 
-  return <UserList length={4} users={users} />;
+  const available = true;
+
+  return <UserList length={4} users={users} isAvailable />;
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,8 +1,11 @@
-const UserList = ({ users, length }) => {
+const UserList = ({ users, length, isAvailable }) => {
+  console.log(isAvailable);
   return (
     <>
       <h1>UserList</h1>
-      <p>{length} Users : </p>
+      <p>
+        {length} Users - {isAvailable ? 'All available' : 'Not available'}
+      </p>
       {users.map(({ id, name, city }) => (
         <div key={id}>
           <h4>{name}</h4>
